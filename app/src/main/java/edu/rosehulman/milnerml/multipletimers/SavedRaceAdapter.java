449bc7a13e55f7094dfa3ec97ab6dce9b7652ffd
theoderic <dsfreak17@gmail.com>
2016-01-27 18:44:58 -0500

    started on RecyclerView needed for the various saved races

---
+++ app/src/main/java/edu/rosehulman/milnerml/multipletimers/SavedRaceAdapter.java
@@ -0,0 +1,42 @@
+package edu.rosehulman.milnerml.multipletimers;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by theoderic on 1/27/16.
+ */
+public class SavedRaceAdapter extends RecyclerView.Adapter<SavedRaceAdapter.ViewHolder> {
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.saved_races, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView mSaveName;
+        private Button mEditButton;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.mSaveName = (TextView)itemView.findViewById(R.id.race_name);
+            this.mEditButton = (Button)itemView.findViewById(R.id.edit_button);
+        }
+    }
+}
