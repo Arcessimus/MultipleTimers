f84abba4cd79752e11dc12e860a6425559539294
theoderic <dsfreak17@gmail.com>
2016-01-27 18:35:31 -0500

    populated settings spinner

--- app/src/main/java/edu/rosehulman/milnerml/multipletimers/SettingsActivity.java
+++ app/src/main/java/edu/rosehulman/milnerml/multipletimers/SettingsActivity.java
@@ -2,6 +2,8 @@ package edu.rosehulman.milnerml.multipletimers;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by kumarms on 1/25/2016.
@@ -11,5 +13,10 @@ public class SettingsActivity extends AppCompatActivity{
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_menu);
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.settings_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
