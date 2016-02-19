f156447f2187ec0dc0f0639945a95e18d0a8fd44
theoderic <dsfreak17@gmail.com>
2016-02-19 04:09:50 -0500

    migrated a bunch of existing to code to start with for the saving UI

--- app/src/main/java/edu/rosehulman/milnerml/multipletimers/SavedRaceAdapter.java
+++ app/src/main/java/edu/rosehulman/milnerml/multipletimers/SavedRaceAdapter.java
@@ -1,17 +1,39 @@
 package edu.rosehulman.milnerml.multipletimers;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by theoderic on 1/27/16.
  */
 public class SavedRaceAdapter extends RecyclerView.Adapter<SavedRaceAdapter.ViewHolder> {
+    private ArrayList<RunnerTime> mTimers = new ArrayList<>();
+    private Context mContext;
+    private Callback mCallBack;
 
+    public ArrayList<RunnerTime> getmTimers() {
+        return mTimers;
+    }
+
+    public SavedRaceAdapter() {
+    }
+
+    public SavedRaceAdapter(Context mContext, Callback callback) {
+        this.mContext = mContext;
+        this.mCallBack = callback;
+    }
+
+    public void add(RunnerTime timer) {
+        mTimers.add(timer);
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -38,5 +60,68 @@ public class SavedRaceAdapter extends RecyclerView.Adapter<SavedRaceAdapter.View
             this.mSaveName = (TextView)itemView.findViewById(R.id.race_name);
             this.mEditButton = (Button)itemView.findViewById(R.id.edit_button);
         }
+    }
+
+    public void update(RunnerTime timer, String name) {
+        timer.setName(name);
+        notifyDataSetChanged();
+    }
+
+    public void remove(RunnerTime timer) {
+        mTimers.remove(timer);
+    }
+
+    public void updateRunnerCards(String currentTime, long updatedTime) {
+        RunnerTime currentRunner;
+        for (int i = 0; i < mTimers.size(); i++) {
+            currentRunner = mTimers.get(i);
+            if (!currentRunner.isClockStopped()) {
+                currentRunner.setClockTime(currentTime);
+            }
+            if (currentRunner.isSplit()){
+                currentRunner.setSplit(false);
+                String splitForLog = currentRunner.addSplit(updatedTime, currentTime);
+                currentRunner.addTotalSplits(currentTime);
+                currentRunner.setLastSplit(splitForLog);
+                currentRunner.getLastSplitsButton().setText(currentRunner.getLastSplit());
+                Log.d("Splits:", "total time: " + currentTime + " |split: " + splitForLog);
+            }
+        }
+    }
+
+    public void resetRunners() {
+        RunnerTime currentRunner;
+        for (int i = 0; i < mTimers.size(); i++) {
+            currentRunner = mTimers.get(i);
+            currentRunner.setClockTime("00:00:00");
+            currentRunner.setUpdatedTime(0);
+            currentRunner.setClockStopped(false);
+            currentRunner.getmSplits().clear();currentRunner.getmTotalTimesSplits().clear();// clear the splits
+            currentRunner.setLastSplit("00:00.000");
+            currentRunner.getLastSplitsButton().setText(currentRunner.getLastSplit());
+        }
+    }
+
+    public class TimerView extends RecyclerView.ViewHolder {
+        private TextView nameView;
+        private Button splitButton;
+        private Button stopButton;
+        private TextView runnerTime;
+        private Button splitsReviewButton;
+
+        public TimerView(View itemView) {
+            super(itemView);
+            nameView = (TextView) itemView.findViewById(R.id.runner_name);
+            runnerTime = (TextView) itemView.findViewById(R.id.runner_time);
+            splitButton = (Button) itemView.findViewById(R.id.split);
+            stopButton = (Button) itemView.findViewById(R.id.stop);
+            splitsReviewButton = (Button) itemView.findViewById(R.id.review_splits);
+        }
+    }
+
+    public interface Callback {
+        public void onEdit(RunnerTime timer);
+
+        void launchReviewDialogCallback(RunnerTime mCurrentTimer);
     }
 }
--- app/src/main/java/edu/rosehulman/milnerml/multipletimers/SavedRacesActivity.java
+++ app/src/main/java/edu/rosehulman/milnerml/multipletimers/SavedRacesActivity.java
@@ -1,25 +1,281 @@
 package edu.rosehulman.milnerml.multipletimers;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Parcelable;
+import android.os.SystemClock;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Timer;
 
 /**
  * Created by kumarms on 1/25/2016.
  */
-public class SavedRacesActivity extends AppCompatActivity{
+public class SavedRacesActivity extends AppCompatActivity implements SavedRaceAdapter.Callback{
     private SavedRaceAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+
+    public TextView time;
+    public long starttime = 0L;
+    public long timeInMilliseconds = 0L;
+    public long timeSwapBuff = 0L;
+    public long updatedtime = 0L;
+    public int startpause = 1;
+    public int secs = 0;
+    public int mins = 0;
+    public int milliseconds = 0;
+    Handler handler = new Handler();
+    private boolean stoprestart;
+    private ArrayList<Parcelable> runnerCards = new ArrayList<>();
+    private int choiceSetting = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        SharedPreferences sharedPref = this.getSharedPreferences("Prefs", Context.MODE_PRIVATE);
+        int choiceTemp = sharedPref.getInt("KEY", 17);
+        Log.d("selected", "" + choiceTemp);
+        if (choiceTemp != 0){
+            choiceSetting = choiceTemp;
+        }
         setContentView(R.layout.saved_races);
 
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setHasFixedSize(true);
         mAdapter = new SavedRaceAdapter();
-        recyclerView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(mAdapter);
+
+        time = (TextView) findViewById(R.id.main_time);
+        final Button butnstart = (Button) findViewById(R.id.main_start);
+        final Button butnstop = (Button) findViewById(R.id.main_stop);
+
+        butnstart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (startpause == 1) {
+                    butnstart.setText("Pause");
+                    butnstop.setText("Stop");
+                    starttime = SystemClock.uptimeMillis();
+                    handler.postDelayed(updateTimer, 0);
+                    startpause = 0;
+                    stoprestart = false;
+                } else {
+                    butnstart.setText("Start");
+                    butnstop.setText("Restart");
+                    time.setTextColor(Color.RED);
+                    timeSwapBuff += timeInMilliseconds;
+                    handler.removeCallbacks(updateTimer);
+                    startpause = 1;
+                    stoprestart = true;
+                }}
+        });
+
+        butnstop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (stoprestart) {// if you press restart then reset all values to their original states
+                    starttime = 0L;
+                    timeInMilliseconds = 0L;
+                    timeSwapBuff = 0L;
+                    updatedtime = 0L;
+                    startpause = 1;
+                    secs = 0;
+                    mins = 0;
+                    milliseconds = 0;
+                    butnstart.setText("Start");
+                    butnstop.setText("Restart");
+                    handler.removeCallbacks(updateTimer);
+                    time.setText("00:00:00");
+                    stoprestart = false;
+                    mAdapter.resetRunners();
+                } else {            // if the clock is going it should just stop
+                    butnstart.setText("Start");
+                    butnstop.setText("Restart");
+                    time.setTextColor(Color.RED);
+                    timeSwapBuff += timeInMilliseconds;
+                    handler.removeCallbacks(updateTimer);
+                    startpause = 1;
+                    stoprestart = true;
+                }
+            }
+        });
+        if (savedInstanceState != null){
+            runnerCards = savedInstanceState.getParcelableArrayList("mRunners");
+            int count = savedInstanceState.getInt("count");
+            for (int i = 0; i<count; i++){
+                mAdapter.add((RunnerTime) runnerCards.get(i));
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        int count = mAdapter.getmTimers().size();
+        for (int i = 0; i<count; i++){
+            runnerCards.add(mAdapter.getmTimers().get(i));
+        }
+        outState.putParcelableArrayList("mRunners", runnerCards);
+        outState.putInt("count", count);
+        // Save off data using outState.putXX(key, value)
+        // Hint: you will use the appropriate methods to store int[] and ints,
+        // maybe a String.
+    }
+
+
+    public Runnable updateTimer = new Runnable() {
+        public void run() {
+
+            timeInMilliseconds = (int) (SystemClock.uptimeMillis() - starttime);
+
+            updatedtime = timeSwapBuff + timeInMilliseconds;
+
+            secs = (int) (updatedtime / 1000);
+            mins = secs / 60;
+            secs = secs % 60;
+            milliseconds = (int) (updatedtime % 1000);
+            String currentTime = "" + mins + ":" + String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds);
+            time.setText(currentTime);
+            time.setTextColor(Color.WHITE);
+            handler.postDelayed(this, 0);
+            mAdapter.updateRunnerCards(currentTime,updatedtime);
+        }
+
+    };
+
+
+    public void showAddEditDialog(final RunnerTime timer) {
+        DialogFragment df = new DialogFragment() {
+            public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle(getString(timer == null ? R.string.dialog_add_title : R.string.dialog_edit_title));
+                View view = getActivity().getLayoutInflater().inflate(R.layout.adding_runner_dialog, null, false);
+                builder.setView(view);
+                final EditText runnerName = (EditText) view.findViewById(R.id.editText);
+                final TextView runnerTime = (TextView)findViewById(R.id.runner_time);
+                if (timer != null) {
+                    // pre-populate
+                    runnerName.setText(timer.getName());
+
+                    TextWatcher textWatcher = new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                            // empty
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+                            // empty
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                            String quote = runnerName.getText().toString();
+                            mAdapter.update(timer, quote);
+                        }
+                    };
+
+                    runnerName.addTextChangedListener(textWatcher);
+                }
+
+                builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (timer == null) {
+                            String name = runnerName.getText().toString();
+                            mAdapter.add(new RunnerTime(name));
+                        }
+                    }
+                });
+                builder.setNeutralButton(getString(R.string.delete), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mAdapter.remove(timer);
+                    }
+                });
+                builder.setNegativeButton(android.R.string.cancel, null);
+
+                return builder.create();
+            }
+        };
+        df.show(getSupportFragmentManager(), "add");
+    }
+
+    @Override
+    public void onEdit(RunnerTime timer) {
+        showAddEditDialog(timer);
+    }
+
+    @Override
+    public void launchReviewDialogCallback(RunnerTime mCurrentTimer) {
+        launchReviewDialog(mCurrentTimer,choiceSetting);
+    }
+
+
+    public class CounterClass extends Timer {
+
+    }
+    private void launchReviewDialog(final RunnerTime runner, final int choice) {
+        DialogFragment df = new DialogFragment() {
+            public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle(runner.getName() + "'s Splits");
+                View view = getActivity().getLayoutInflater().inflate(R.layout.splits_dialog_layout, null, false);
+                builder.setView(view);
+                final TextView lap = (TextView) view.findViewById(R.id.runner_name_for_dialog);
+                lap.setText(R.string.lap_number);
+                RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.splits_recycler_view);
+                SplitsAdapter adapter = new SplitsAdapter(choice);
+                recyclerView.setAdapter(adapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+                for (int i = 0; i < runner.getmSplits().size(); i++){
+                    adapter.addSplits(runner.getmSplits().get(i),runner.getmTotalTimesSplits().get(i));
+                }
+//                builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        if (timer == null) {
+//                            String name = runnerName.getText().toString();
+//                            mTimerAdapter.add(new RunnerTime(name));
+//                        }
+//                    }
+//                });
+//                builder.setNeutralButton(getString(R.string.delete), new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        mTimerAdapter.remove(timer);
+//                    }
+//                });
+                builder.setNegativeButton(android.R.string.ok, null);// just need one button to exit
+
+                return builder.create();
+            }
+        };
+        df.show(getSupportFragmentManager(), "add");
     }
 }
